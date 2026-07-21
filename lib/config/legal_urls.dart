20319a64c663690bc7e0f569421ecb3691baa935
mburhankhan101-glitch <m.burhan.khan101@gmail.com>
2026-07-21 16:21:10 +0500

    pdated URLs

--- lib/config/legal_urls.dart
+++ lib/config/legal_urls.dart
@@ -3,8 +3,8 @@
 /// Hosted on Firebase Hosting (`web_docs/`). Redeploy with:
 ///   firebase deploy --only hosting
 abstract final class LegalUrls {
-  static const privacyPolicy = 'https://bazaar-connect-c4487.web.app/privacy';
-  static const termsOfService = 'https://bazaar-connect-c4487.web.app/terms';
-  static const accountDeletion = 'https://bazaar-connect-c4487.web.app/delete';
+  static const privacyPolicy = 'https://thokbazaar-4a79a.web.app/privacy';
+  static const termsOfService = 'https://thokbazaar-4a79a.web.app/terms';
+  static const accountDeletion = 'https://thokbazaar-4a79a.web.app/delete';
   static const supportEmail = 'raspharmaceutical51@gmail.com';
 }
